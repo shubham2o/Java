070f587b625ce78dbe7abdbb83b5e7c28f9087eb
shubham2o <shubhamsharma.ha@gmail.com>
2022-06-26 06:27:41 +0530

    Added 3rd and 4th approach using for loop

--- First-Java-Program/src/com/shubham/Fibonacci.java
+++ First-Java-Program/src/com/shubham/Fibonacci.java
@@ -7,8 +7,8 @@ public class Fibonacci {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
-        // 1st Approach
-        System.out.println("A. 1st Approach");
+        // 1st Approach - using while loop
+        System.out.println("A. 1st Approach using while loop");
         int a = 0;
         int b = 1;
         int count = 2;
@@ -21,8 +21,8 @@ public class Fibonacci {
         }
         System.out.println(b);
 
-        // 2nd Approach
-        System.out.println("B. 2nd Approach");
+        // 2nd Approach - using while loop
+        System.out.println("B. 2nd Approach using while loop");
         int first = 1;
         int second = 1;
         int counter = 3;
@@ -34,5 +34,29 @@ public class Fibonacci {
             counter++;
         }
         System.out.println(second);
+
+        // 3rd Approach - using for loop
+        System.out.println("C. 3rd Approach using for loop");
+        int x = 0;
+        int y = 1;
+
+        for (int counting = 2; counting <= n; counting++) {
+            int temp1 = y;
+            y = x +  y;
+            x = temp1;
+        }
+        System.out.println(y);
+
+        // 4th Approach - using for loop
+        System.out.println("D. 4th Approach using for loop");
+        int r = 1;
+        int s = 1;
+
+        for (int nth = 3; nth <= n; nth++) {
+            int addition = r + s;
+            r = s;
+            s = addition;
+        }
+        System.out.println(s);
     }
 }
