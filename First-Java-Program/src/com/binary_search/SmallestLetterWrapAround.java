9cd18c58b4d5ce304e37a29cc1217c2fd6291304
shubham2o <shubhamsharma.ha@gmail.com>
2022-07-08 23:46:30 +0530

    new program - SmallestLetterWrapAround added

---
+++ First-Java-Program/src/com/binary_search/SmallestLetterWrapAround.java
@@ -0,0 +1,33 @@
+package com.binary_search;
+
+public class SmallestLetterWrapAround {
+     /* Find The Smallest Letter Greater Than Target
+    Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest
+    character in the array that is larger than target.
+    NOTE: The letters wrap around.
+    • For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+
+    EXAMPLE 1:
+    Input: letters = ["c", "f", "j"], target = "a"
+    Output: "c"
+
+    EXAMPLE 2:
+    Input: letters = ["c", "f", "j"], target = "c"
+    Output: "f"
+
+    EXAMPLE 3:
+    Input: letters = ["c", "f", "j"], target = "d"
+    Output: "f"
+
+    EXAMPLE 4:
+    Input: letters = ["c", "f", "j"], target = "g"
+    Output: "j"
+
+    EXAMPLE 5:
+    Input: letters = ["c", "f", "j"], target = "j"
+    Output: "c" */
+
+    public static void main(String[] args) {
+
+    }
+}
